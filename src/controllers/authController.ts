895c38becb704a78177147f0d27274eec8a5a9a0
mathferreira01 <ferreiramatheus124@gmail.com>
2023-11-24 17:03:10 -0300

    cria função login do usuário

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -59,5 +59,54 @@ export async function register(req: Request, res: Response) {
 }
 
 export async function login(req: Request, res: Response) {
-  // ... (o mesmo código que você já possui)
+  try {
+    const { email, senha } = req.body;
+
+    // Validations
+    if (!email || !senha) {
+      return res.status(422).json({ msg: 'Email e senha são obrigatórios!' });
+    }
+
+    // Verificar se o usuário existe
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({ msg: 'Usuário não encontrado!' });
+    }
+
+    // Verificar se a senha está correta
+    const checkPassword = await bcrypt.compare(senha, user.passwordHash);
+
+    if (!checkPassword) {
+      return res.status(422).json({ msg: 'Senha inválida' });
+    }
+
+    // Atualizar a última data de login
+    user.ultimo_login = new Date();
+    await user.save();
+
+    // Gerar token JWT
+    const secret = process.env.SECRET || '';
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+
+    // Formatar a resposta
+    const output = {
+      id: user._id,
+      data_criacao: user.createdAt,
+      data_atualizacao: user.updatedAt,
+      ultimo_login: user.ultimo_login,
+      token: token,
+    };
+
+    res.status(200).json(output);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Erro interno do servidor' });
+  }
 }
+
