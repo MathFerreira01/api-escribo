dc562d1f32c7ff40afa7d979ed09d05f1337bcd8
mathferreira01 <ferreiramatheus124@gmail.com>
2023-11-24 16:13:49 -0300

    cria as rotas como a principal, login e registro

---
+++ src/controllers/authController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel';
+
+export async function register(req: Request, res: Response) {
+  // ... (o mesmo código que você já possui)
+}
+
+export async function login(req: Request, res: Response) {
+  // ... (o mesmo código que você já possui)
+}
